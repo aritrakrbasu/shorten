6e107ec102f0420134f68b1bb746c24650f80731
Aritra Basu <runabasu71@gmail.com>
2020-05-06 14:48:57 +0530

    Basic fixes

--- src/firebase_config.js
+++ src/firebase_config.js
@@ -1,17 +1,17 @@
 import firebase from 'firebase';
 import 'firebase/firestore'
-
+//put all your firbase details below
 	const config = {
-    apiKey: "AIzaSyDUl5oJC6k3rUPIzT6Ltr2FYelx1ftfiOg",
-    authDomain: "testshorten-bf5f4.firebaseapp.com",
-    databaseURL: "https://testshorten-bf5f4.firebaseio.com",
-    projectId: "testshorten-bf5f4",
-    storageBucket: "testshorten-bf5f4.appspot.com",
-    messagingSenderId: "113968819419",
-    appId: "1:113968819419:web:d5a1dc54c0b4396584f0fe",
-    measurementId: "G-JRNFWZR7F5"
+    apiKey: "#####",
+    authDomain: "#####",
+    databaseURL: "#####",
+    projectId:"#####",
+    storageBucket: "#####",
+    messagingSenderId: "#####",
+    appId: "#####",
+    measurementId: "#####"
       };
     export const fire=firebase.initializeApp(config);
     export const db=firebase.firestore();
-    export const domain = 'http://localhost:3000';
+    export const domain = 'http://localhost:3000'; //put your domain here 
     export default firebase; 
